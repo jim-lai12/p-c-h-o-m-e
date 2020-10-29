f1306a4cdefdb3b9c30d807ae236e95ec41f1199
yatogamitohka1212 <70970939+yatogamitohka1212@users.noreply.github.com>
2020-10-29 23:31:27 +0800

    fix
    fix

--- tool/getconfig.py
+++ tool/getconfig.py
@@ -79,109 +79,12 @@ class Config:
             if re.search("20+[2,3]{1}[0-9]{1}[0,1]{1}[0-9]{1}", self.cardExpireDate) == None:
                 Error += 1
                 logging.error("cardExpireDate error")
-            self.orderData = {
-                'PayWay': 'CC',  # 付款方式，COD為貨到付款。CC信用卡
-                'CusName': self.cusName,  # 購買人姓名。
-                'CusMobile': self.cusMobile,  # 購買人連絡電話 - 手機。
-                'AcceptEDM': 'N',  # 是否願意收到PChome商品特惠通知。
-                'CusTel': '',  # 購買人連絡電話 - 市話。
-                'CusZip': self.cusZip,  # 購買人郵遞區號。
-                'CusAddress': self.cusAddress,  # 購買人地址。
-                'ShowCusName': 'N',  # 收貨地址顯示購買人姓名。
-                'ContactNo': '',
-                'isSyncCust': 'N',
-                'RecName': self.recName,  # 收貨人中文姓名。
-                'RecTel': '',  # 收貨人連絡電話 - 市話。
-                'RecMobile': self.recMobile,  # 收貨人連絡電話 - 手機。
-                'RecZip': self.recZip,  # 收貨人郵遞區號。
-                'RecAddress': self.recAddress,  # 收貨人地址。
-                'AddContact': 'N',  # 資料加入收貨人通訊錄。
-                'ConfirmIsLand': 'N',
-                'RecMail': '',
-                'PaperInvoice': 'N',  # 是否願意將發票進行捐贈。
-                'InvoiceType': 'P',  # 個人電子發票。
-                'TaxNO': '',  # 發票種類為公司戶電子發票時，統一編號。
-                'AddTaxNO': 'N',  # 資料加入公司統編備忘錄。
-                'CashPoint': '0',
-                'DeviceID': '',
-                'DeviceOS': '',
-                'DeviceName': '',
-                'DeviceOSVersion': '',
-                'DeviceAppVersion': '',
-                'IsSkipOTP': 'N',
-                'availableDepositPoint': '0',
-                'availableVoucherPoint': '0',
-                'depositUsed': '0',
-                'voucherUsed': '0',
-                'BindMobile': '',
-                # -----------------------信用卡才要
-                "RememberCard": "N",  # 記得卡號
-                "CardNO": self.cardNO,  # 卡號
-                "useRealName": "N",  # 購買同會員資訊
-                "CardType": self.cardType,  # 卡別
-                "CusBirthday": self.cusBirthday,  # 持卡人生日
-                "CusSID": self.cusSID,  # 持卡人身分證字號
-                "CusGender": self.cusGender,  # 持卡人性別
-                "CardSecurityNO": self.cardSecurityNO,  # 後三碼
-                "CardExpireDate": self.cardExpireDate,  # 卡片期限
-                "CardMac": "",
-                "BankId": "",
-                "BUID": "",
-                "ship_type": "delivery",  # delivery是貨運
-                "ship_only": "N",
-
-            }
-        elif self.payWay == "home":
-            self.orderData = {
-                'PayWay': 'COD',  # 付款方式，COD為貨到付款。CC信用卡
-                'CusName': self.cusName,  # 購買人姓名。
-                'CusMobile': self.cusMobile,  # 購買人連絡電話 - 手機。
-                'AcceptEDM': 'N',  # 是否願意收到PChome商品特惠通知。
-                'CusTel': '',  # 購買人連絡電話 - 市話。
-                'CusZip': self.cusZip,  # 購買人郵遞區號。
-                'CusAddress': self.cusAddress,  # 購買人地址。
-                'ShowCusName': 'N',  # 收貨地址顯示購買人姓名。
-                'ContactNo': '',
-                'isSyncCust': 'N',
-                'RecName': self.recName,  # 收貨人中文姓名。
-                'RecTel': '',  # 收貨人連絡電話 - 市話。
-                'RecMobile': self.recMobile,  # 收貨人連絡電話 - 手機。
-                'RecZip': self.recZip,  # 收貨人郵遞區號。
-                'RecAddress': self.recAddress,  # 收貨人地址。
-                'AddContact': 'N',  # 資料加入收貨人通訊錄。
-                'ConfirmIsLand': 'N',
-                'RecMail': '',
-                'PaperInvoice': 'N',  # 是否願意將發票進行捐贈。
-                'InvoiceType': 'P',  # 個人電子發票。
-                'TaxNO': '',  # 發票種類為公司戶電子發票時，統一編號。
-                'AddTaxNO': 'N',  # 資料加入公司統編備忘錄。
-                'CashPoint': '0',
-                'DeviceID': '',
-                'DeviceOS': '',
-                'DeviceName': '',
-                'DeviceOSVersion': '',
-                'DeviceAppVersion': '',
-                'IsSkipOTP': 'N',
-                'availableDepositPoint': '0',
-                'availableVoucherPoint': '0',
-                'depositUsed': '0',
-                'voucherUsed': '0',
-                'BindMobile': '',
-                "ship_type": "delivery",  # delivery是貨運
-                "ship_only": "N",
-            }
         else:
             Error += 1
             logging.error("payWay error")
         if Error != 0:
             logging.error("Have "+str(Error)+" error in config.")
             sys.exit()
-        if self.cusMobile[0:2] != "09":
-            self.orderData["CusMobile"] = ""
-            self.orderData["CusTel"] = self.cusMobile
-        if self.recMobile[0:2] != "09":
-            self.orderData["RecMobile"] = ""
-            self.orderData["RecTel"] = self.recMobile
     def savecookie(self,s,cookiefile = "cookie.txt"):
         cj = cookielib.MozillaCookieJar()
         for s_cookie in s.cookies:
